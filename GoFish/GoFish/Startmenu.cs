d9442bf7f62bb59641d096225b43e600b4e132c0
ElliotSeger <elliot.seger@hotmail.com>
2018-11-02 17:03:20 +0100

    ok

--- GoFish/GoFish/Startmenu.cs
+++ GoFish/GoFish/Startmenu.cs
@@ -14,22 +14,20 @@ namespace CardGameFish
 
             string begin = "Enter för att börja spelet";
             string exit = "Escape för att avsluta spelet";
-            Console.SetCursorPosition((Console.WindowWidth - begin.Length) / 2, 5);
+            Console.SetCursorPosition((Console.WindowWidth - begin.Length) / 2, 10);
             Console.WriteLine(begin);
-            Console.SetCursorPosition((Console.WindowWidth - exit.Length) / 2, 6);
+            Console.SetCursorPosition((Console.WindowWidth - exit.Length) / 2, 11);
             Console.WriteLine(exit);
 
             ConsoleKeyInfo s = Console.ReadKey();
-
 
             if (s.Key == ConsoleKey.Enter)
             {
                 Console.Clear();
                 string players = "Skriv in antal spelare från 2 till 4";
-                List<int> nbrOfPlayers = new List<int>();
                 Console.SetCursorPosition((Console.WindowWidth - players.Length) / 2, 5);
                 Console.WriteLine(players);
-
+                
                 Console.SetCursorPosition((Console.WindowWidth - players.Length) / 2, 6);
             }
 
